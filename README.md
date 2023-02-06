3f89fdbbc128327ff7888fa4215b9f41a1429d62
Yuan-Yao Lou <yyloumike@gmail.com>
2023-02-05 19:08:14 -0500

    Re-org (Feb 05, 2023)

--- README.md
+++ README.md
@@ -1,30 +1,11 @@
-# Python Coding
+# Coding Practice
+> [Notion Pages](https://yylou.notion.site/CODING-eef2bf79104b44709c56f0bedea8d9f5)
 
-* ### [Basic](./_basic.md)
-    * Math Operators
-    * Operator Precedence
-    * Exponential Notation Format
-    * Complex Numbers
-    * Function Objects vs. Callables
-* ### [Class](./_class.md)
-    * Classes, Instances, and Attributes
-    * Pre-Defined Attributes for a Class
-    * Pre-Defined Attributes for an Instance
-    * How Python Creates an Instance from a Class
-    * Destruction of Instance Objects
-    * Encapsulation, Inheritance, and Polymorphism
-    * Advantages of Inheritance
-    * Method Overriding, Operator Overloading
-    * Implementing duck typing
-    * Abstract Base Classes (ABC)
-    * Iterable vs. Iterator
-* ### [Module](./_module.md)
-    * Argument Parsing
-    * Collections Module
-* ### Data Structures (w/ Leetcode Problems)
-    * [Hash Table ```#8```](./_HashTable.md)
-    * [Singly Linked List ```#30```](./_SinglyLinkedList.md)
-    * [Circular Linked List ```#3```](./_CircularLinkedList.md)
-    * [Doubly Linked List](./_DoublyLinkedList.md)
-* ### Blind 75 LeetCode Problems
-    * [Week 1 ```#12```](./__Blind75-Week1.md)
+* ### Python Programming
+    * ```Educative.io``` [Python for Programmers](https://www.educative.io/path/python-for-programmers)
+    * ```Educative.io``` [Ace The Python Coding Interview](https://www.educative.io/path/ace-python-coding-interview)
+
+* ### Problem Solving
+    * Bling 75
+    * NeetCode
+    * LeetCode
---
+++ parse.py
@@ -0,0 +1,56 @@
+"""
+Author  : Yuan-Yao Lou (Mike)
+Title   : PhD student in ECE at Purdue University
+Email   : yylou@purdue.edu
+Website : https://yylou.github.io/
+Date    : 2023/01/22
+
+Describtion : Parse HTML (Leetcode: Company tags)
+"""
+
+from bs4        import BeautifulSoup
+import json     as      JSON
+
+class Problem:
+    def __init__(self, id=0, title="None", href="/", tags="None", level="Easy|Medium|Hard", freq="0.0"):
+        self.id     = id
+        self.title  = title
+        self.href   = href
+        self.tags   = tags
+        self.level  = level
+        self.freq   = freq
+    
+    def __str__(self): return JSON.dumps(self.__dict__, indent=4)
+
+class Company:
+    def __init__(self, path):
+        self.object = BeautifulSoup(open(path, encoding="utf-8"), features="html.parser")
+        self.table  = self.object.find("tbody", attrs={"class": "reactable-data"})
+        self.id     = self.table.find_all("td", attrs={"label": "#"})
+        self.title  = self.table.find_all("td", attrs={"label": "Title"})
+        self.href   = [title.a for title in self.title]
+        self.tags   = self.table.find_all("td", attrs={"label": "Tags"})
+        self.level  = self.table.find_all("td", attrs={"label": "Difficulty"})
+        self.freq   = self.table.find_all("td", attrs={"label": "Frequency,[object Object]"})
+        self.filter = {"level": {},
+                       "tags":  {}}
+        self.problems = {}
+        self.sorted = []
+        for i in range(len(self.id)):
+            id      = "0"*(4 - len(self.id[i].attrs["value"])) + self.id[i].attrs["value"]
+            title   = self.title[i].attrs["value"]
+            href    = f"https://leetcode.com{self.href[i].attrs['href']}"
+            tags    = [tag.attrs["title"] for tag in self.tags[i].find_all("a")]
+            level   = ' '.join(self.level[i].span.contents)
+            freq    = self.freq[i].attrs["value"]
+            
+            self.problems[id] = Problem(id=id, title=title, href=href, tags=tags, level=level, freq=freq)
+            self.sorted.append(Problem(id=id, title=title, href=href, tags=tags, level=level, freq=freq))
+            
+            self.filter["level"][level] = self.filter["level"].get(level, []) + [id]
+            for tag in tags: self.filter["tags"][tag] = self.filter["tags"].get(tag, []) + [id]
+
+    def get_problem(self, id: str): return self.problems[id]
+    def get_problems(self): return [prob.id for prob in sorted(self.problems.values(), key=lambda x: x.freq, reverse=True)]
+    def get_tags(self):     return [tag[0] for tag in sorted(self.filter["tags"].items(), key=lambda x: len(x[1]), reverse=True)]
+    def get_levels(self):   return [(level[0], level[1]) for level in sorted(self.filter["level"].items(), key=lambda x: len(x[1]), reverse=True)]
