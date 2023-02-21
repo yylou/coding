5a7dfb7cf3086e318a4d53344b28bfed84dab8df
Yuan-Yao Lou <yyloumike@gmail.com>
2023-02-21 01:04:02 -0500

    String (Feb 21, 2023)

--- String.py
+++ String.py
@@ -18,6 +18,61 @@ class String:
         return None
 
     @classmethod
+    def _0020_valid_parentheses(self, s: str) -> bool:
+        """  Easy  |  Stack  """
+        # Time:  O(n)
+        # Space: O(n)
+
+        mapping = {"]": "[", ")": "(", "}": "{"}
+        stack = []
+        for char in s:
+            if char in mapping:
+                if not stack: return False  # un-opened
+                if stack.pop() != mapping[char]: return False   # un-matched
+            else:
+                stack.append(char)
+        
+        if stack: return False      # un-closed
+        return True
+
+    @classmethod
+    def _0022_generate_parentheses(self, n: int) -> list[str]:
+        def solution_1(n):
+            """  Medium  |  BFS  """
+            # Time:  O(n * m * length)
+            # Space: O(m)
+
+            ans = ["()"]
+            for i in range(1, n):
+                tmp = set()
+                for string in ans:
+                    for idx in range(len(string)):
+                        tmp.add(f"{string[:idx]}{'()'}{string[idx:]}")
+                ans = tmp
+
+            return ans
+
+        def solution_2(n):
+            """  Medium  |  DFS, Backtracking  """
+            # Time:  O()
+            # Space: O()
+            
+            ans = []
+            def dfs(string: str, left: int, right: int):
+                if n == left == right:
+                    ans.append(string)
+                
+                else:
+                    if left < n: dfs(string + '(', left + 1, right)
+                    if right < left: dfs(string + ')', left, right + 1)
+
+            dfs("", 0, 0)
+            return ans
+
+        return solution_1(n=n)
+        return solution_2(n=n)
+
+    @classmethod
     def _0125_valid_palindrome(self, s: str) -> bool:
         """  Easy  |  Two Pointer  """
         # Time:  O(n)
