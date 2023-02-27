139f0b0ecaaba4414fbba4cb8d2d63067b3adc9e
Yuan-Yao Lou <yyloumike@gmail.com>
2023-02-27 00:25:03 -0500

    System (Feb 27, 2023)

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ Project :
 import argparse                 as ARGU
 import inspect
 import os                       as OS
+import re                       as RE
 
 from Array                      import Array
 from String                     import String
@@ -25,6 +26,7 @@ from Test                       import Test
 def argu():
     parser = ARGU.ArgumentParser(description="LeetCode Problem Solving DB")
     parser.add_argument("-prob",     nargs='?', type=str,    default="",    help="(problem id)")
+    parser.add_argument("-list",     action='store_true',                   help="(solved problems)")
     return parser
 
 def get_code(data): print('\n\n' + inspect.getsource(data) if data else "")
@@ -32,11 +34,46 @@ def get_test(id: str, obj: object, func: object): Test.search(id, obj, func); pr
 
 def main():
     argp = argu().parse_args()
-    if argp.prob:
-        data  = {}
-        files = OS.listdir("./company")
-        for file in files: data[file] = Company(path=f"./company/{file}")
+    data  = {}
+    files = OS.listdir("./company")
+    for file in files: data[file] = Company(path=f"./company/{file}")
+    
+    HIFREQ = Colors.Pattern(Colors.BOLD, Colors.TLRED, Colors.BBLACK)
+    YELLOW = Colors.Pattern(Colors.BOLD, Colors.TYELLOW, Colors.BNONE)
+    
+    table = {
+        "Dynamic Programming":  "DP",
+        "Breadth-First Search": "BFS",
+        "Depth-First Search":   "DFS",
+        "Binary Search Tree":   "BST",
+    }
 
+    if argp.list:
+        print(f"\n    {YELLOW}{'ID':^8} {'Title':^60} {'Level':<15}{'Tags':<50}{'Company'}{Colors.END}")
+        print("    " + "=====" * 34)
+
+        for element in [Array, String, Stack, Tree]:
+            for problem_id in element.__dict__:
+                result = RE.match("_(\d*)_(.*)", problem_id)
+                if result and result.groups()[0]:
+                    idx = result.groups()[0]
+                    info = {"company": {"amazon": 0, "apple": 0, "bloomberg": 0, "facebook": 0, "google": 0, "microsoft": 0}}
+                    for name, company in data.items():
+                        if idx in company.get_problems():
+                            info |= company.get_problem(idx).__dict__
+                            info["company"][name[:-5]] = info['freq'] / company.sorted[0].freq * 100
+                            del info["freq"]
+                    
+                    if any([_ > 50 for _ in info["company"].values()]): 
+                        print(f"    {HIFREQ}{idx:^8} {info['title']:60}{Colors.END} {info['level']:<15}", end="")
+                    else: 
+                        print(f"    {idx:^8} {info['title']:60} {info['level']:<15}", end="")
+                    print(f"{' / '.join(sorted([table[_] if _ in table else _ for _ in info['tags'][:3]])):<50}", end="")
+                    print(f"{' | '.join([k.capitalize()[:3] if v != 0 else '   ' for k, v in sorted(info['company'].items())])}")
+            print("    " + "=====" * 34)
+
+
+    if argp.prob:
         #   | Collect
         info = {"company": []}
         for name, company in data.items():
@@ -47,9 +84,9 @@ def main():
 
         #   | Output
         info["company"].sort(key=lambda x: eval(x[1]), reverse=True)
-        CYAN = Colors.Pattern(Colors.BOLD, Colors.TCYAN, Colors.BNONE)
+        CYAN = Colors.Pattern(Colors.BOLD, Colors.TCYAN,  Colors.BNONE)
         GREEN= Colors.Pattern(Colors.BOLD, Colors.TGREEN, Colors.BNONE)
-        RED  = Colors.Pattern(Colors.BOLD, Colors.TRED, Colors.BNONE)
+        RED  = Colors.Pattern(Colors.BOLD, Colors.TRED,   Colors.BNONE)
         GREY = Colors.Pattern(Colors.BOLD, Colors.TDGREY, Colors.BNONE)
         END  = Colors.END
         TAB  = "    "
